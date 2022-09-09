03816a37e1f71b0de29a60057c83076aaec38611
azuzCoder <go.mega.uz@gmail.com>
2022-09-09 23:10:18 +0500

    added ckeditor

--- MyBlog/settings.py
+++ MyBlog/settings.py
@@ -37,6 +37,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+
+    'ckeditor',
+
     'accounts.apps.ConfigConfig',
 ]
 
@@ -119,6 +122,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 STATICFILES_DIRS = [BASE_DIR / 'static/']
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
